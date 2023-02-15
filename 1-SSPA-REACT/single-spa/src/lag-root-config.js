188c2f486c375e5bc5b5b9f44f294876371dcc42
Aguiar16 <lucas16k9@gmail.com>
2023-02-15 20:42:46 -0300

    SSPA + REACT (not working yet)

---
+++ 1-SSPA-REACT/single-spa/src/lag-root-config.js
@@ -0,0 +1,26 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    System.import(
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: ["/"],
+});
+
+// registerApplication({
+//   name: "@lag/react-single",
+//   app: () =>
+//     System.import("@lag/react-single"),
+//   activeWhen: ["/react-single"],
+// });
+// registerApplication({
+//   name: "@lag/navbar",
+//   app: () => System.import("@lag/navbar"),
+//   activeWhen: ["/"]
+// });
+
+start({
+  urlRerouteOnly: true,
+});
